611209edca04adaa39dd41edfe3b8088a412bcd0
AK47-23 <anurudhmanosh123@gmail.com>
2022-09-04 10:49:51 +0530

    init

--- lib/utilities/size_config.dart
+++ lib/utilities/size_config.dart
@@ -12,16 +12,16 @@ class SizeConfig {
   static double safeBlockVertical = 0;
 
   void init(BuildContext context) {
-    MediaQueryData _mediaQueryData = MediaQuery.of(context);
-    screenWidth = _mediaQueryData.size.width;
-    screenHeight = _mediaQueryData.size.height;
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    screenWidth = mediaQueryData.size.width;
+    screenHeight = mediaQueryData.size.height;
     blockSizeHorizontal = screenWidth / 100;
     blockSizeVertical = screenHeight / 100;
 
     _safeAreaHorizontal =
-        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+        mediaQueryData.padding.left + mediaQueryData.padding.right;
     _safeAreaVertical =
-        _mediaQueryData.padding.top + _mediaQueryData.padding.bottom;
+        mediaQueryData.padding.top + mediaQueryData.padding.bottom;
     safeBlockHorizontal = (screenWidth - _safeAreaHorizontal) / 100;
     safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
   }
